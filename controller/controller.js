cf838f2bdcf1671c69bf8329699c351313a8f19a
Dania <o.dania@icloud.com>
2021-08-13 09:42:20 +0200

    added new route

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ success: true, data: 'I am healthy!' });
+    res.status(200).json({ success: true, data: 'Welcome to Coda Payment Sandbox' });
 });
+
+
+router.get('/health', (req, res) => {
+    res.status(200).json({ success: true, data: 'Yay! I am healthy!' });
+});
+
 
 module.exports = router;
